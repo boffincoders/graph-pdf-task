cb846ff89d89c0d0fea23f99be5df3c26041a8d3
Atul <atul_kumar@boffincoders.com>
2023-10-13 11:49:57 +0530

    URL Changes

--- src/utils/apiEndpoints.ts
+++ src/utils/apiEndpoints.ts
@@ -2,7 +2,7 @@
 export const ApiEndpoints = {
 
     GRAPH_DATA: 'https://api.usa.gov/crime/fbi/cde/arrest/state/AK/all?from=2015&to=2020&API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv',
-    GENERATE_PDF_URL: 'http://localhost:9000/api/generate-pdf'
+    GENERATE_PDF_URL: 'https://graph-pdf-task-backend.onrender.com/api/generate-pdf'
 
 
 }
